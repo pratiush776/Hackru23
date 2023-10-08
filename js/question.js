71639bfe62868b7ed284b0bc3b4204f13e5615f1
pratiush776 <pratiush776@gmail.com>
2023-10-08 11:46:47 -0400

    draft 2:lander hyperlink fix

--- js/question.js
+++ js/question.js
@@ -1,71 +1,13 @@
-const questions = [
-    {
-        question: 'What keyword is used to declare a variable in JavaScript? Choose all the correct answer.',
-        answers: [
-            { text: "let", correct: true },
-            { text: "variable", correct: false },
-            { text: "const", correct: true },
-            { text: "var", correct: true },
-        ]
-    },
-    {
-        question: 'In JavaScript, which of the following is not a primitive data type?',
-        answers: [
-            { text: "Answer 1", correct: true },
-            { text: "Answer 2", correct: false },
-            { text: "Answer 3", correct: false },
-            { text: "Answer 4", correct: false },
-        ]
-    },
-    {
-        question: 'Enter Your Email',
-        answers: [
-            { text: "Answer 1", correct: true },
-            { text: "Answer 2", correct: false },
-            { text: "Answer 3", correct: false },
-            { text: "Answer 4", correct: false },
-        ]
-    },
-    {
-        question: 'Enter Your Phone Number',
-        answers: [
-            { text: "Answer 1", correct: true },
-            { text: "Answer 2", correct: false },
-            { text: "Answer 3", correct: false },
-            { text: "Answer 4", correct: false },
-        ]
-    },
-    {
-        question: 'Enter Your Address',
-        answers: [
-            { text: "Answer 1", correct: true },
-            { text: "Answer 2", correct: false },
-            { text: "Answer 3", correct: false },
-            { text: "Answer 4", correct: false },
-        ]
-    },
-    {
-        question: 'Enter Your City',
-        answers: [
-            { text: "Answer 1", correct: true },
-            { text: "Answer 2", correct: false },
-            { text: "Answer 3", correct: false },
-            { text: "Answer 4", correct: false },
-        ]
-    },
-    {
-        question: 'Enter Your State',
-        answers: [
-            { text: 'Answer 1', correct: true },
-            { text:'Answer 2', correct: false },
-            { text: 'Answer 3', correct: false },
-            { text: 'Answer 4', correct: false },
-        ]
+fetch("/json/computer.json")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
-
-];
-
-const questionElement = document.getElementById('question')
+    return response.json();
+  })
+  .then(data => {
+    const questions=data.easy
+    const questionElement = document.getElementById('question')
 const answerButtons= document.getElementById('answer-buttons')
 const nextButton = document.getElementById('next-btn')
 
@@ -150,3 +92,9 @@ nextButton.addEventListener('click', () => {
 });
 
 startQuiz();
+  })
+  .catch(error => {
+    // Handle any errors here
+    console.error(error);
+  });
+
